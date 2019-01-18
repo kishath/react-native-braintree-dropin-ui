88046b557fb25c76e18a18f483642265ff6bffef
Kishath <kishath@skoolbag.com.au>
2019-01-18 17:41:36 +0530

    added support for ios

--- ios/RNBraintreeDropIn.m
+++ ios/RNBraintreeDropIn.m
@@ -62,6 +62,10 @@ RCT_REMAP_METHOD(show,
         request.applePayDisabled = YES;
     }
 
+    if(![options[@"payPal"] boolValue]){
+        self.paypalDisabled = YES;
+    }
+
     BTDropInController *dropIn = [[BTDropInController alloc] initWithAuthorization:clientToken request:request handler:^(BTDropInController * _Nonnull controller, BTDropInResult * _Nullable result, NSError * _Nullable error) {
             [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
 
