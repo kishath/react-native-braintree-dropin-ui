59866847c71819aa1debbb1b091e887b4cb1b9ea
Kishath <kishath@skoolbag.com.au>
2019-01-19 01:16:19 +0530

    reverted ios chages

--- ios/RNBraintreeDropIn.m
+++ ios/RNBraintreeDropIn.m
@@ -62,10 +62,6 @@ RCT_REMAP_METHOD(show,
         request.applePayDisabled = YES;
     }
 
-    if(![options[@"payPal"] boolValue]){
-        self.paypalDisabled = YES;
-    }
-
     BTDropInController *dropIn = [[BTDropInController alloc] initWithAuthorization:clientToken request:request handler:^(BTDropInController * _Nonnull controller, BTDropInResult * _Nullable result, NSError * _Nullable error) {
             [self.reactRoot dismissViewControllerAnimated:YES completion:nil];
 
